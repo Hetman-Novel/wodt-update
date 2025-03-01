9f96b8f49e1dfb0fab7e23b53e91568ba9cb26ec
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2025-03-01 22:18:45 +0200

    Add files via upload

--- files/js/loading.js
+++ files/js/loading.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
    let preloader = document.getElementById('preloader');
-   if (preloader) {
-      let loadTime = performance.now(); // Page load time in milliseconds
-      let delay = Math.max(2000 - loadTime, 0); // Calculate the delay
+   let loadTime = performance.now(); // Время загрузки страницы в миллисекундах
+   let delay = Math.max(3000 - loadTime, 0); // Рассчитываем, нужна ли задержка
 
-      if (preloader) {
-         setTimeout(() => {
-            preloader.classList.add('loaded');
-         }, delay);
-      }
+   if (preloader) {
+      setTimeout(() => {
+         preloader.classList.add('loaded');
+      }, delay);
    }
 });
